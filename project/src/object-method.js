aa25ccbfbde9e26165f3711a5966418e11eb2257
tiit_ASUS <go.tiit.ogle@gmail.com>
2023-12-27 18:28:20 +0300

    45sec

--- project/src/object-method.js
+++ project/src/object-method.js
@@ -10,8 +10,21 @@ const quitarPlayer = {
     },
 }
 
-// Создаем массив пар
-const keys = Object.entries(quitarPlayer)
+// Создаем массив массивов пар *** ключ / значение
+const keys_values = Object.entries(quitarPlayer)
+
+// Создаем массив пар ***  значение
+const values = Object.values(quitarPlayer)
+
+// Создаем массив пар ***  ключ
+const keys = Object.keys(quitarPlayer)
+
+// Получаем все методы (для глубокого перебора используем for in вместо of)
+for (let propertyName of keys){
+    if (typeof quitarPlayer[propertyName] === `function`){
+        quitarPlayer[propertyName]();
+    }
+}
 
 export const objectMethod = () => {
     return keys
