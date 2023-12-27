b5152c9354daadb3153d542cd36556bf72deeb3f
tiit_ASUS <go.tiit.ogle@gmail.com>
2023-12-27 12:08:15 +0300

    001

--- project/src/main.js
+++ project/src/main.js
@@ -103,8 +103,8 @@ const createFilterTemplate = () => {
 };
 
 const createBoardTemplate = () => {
-    return (
-        `      <section class="board container">
+  return (
+    `      <section class="board container">
         <div class="board__filter-list">
           <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
           <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
@@ -119,8 +119,8 @@ const createBoardTemplate = () => {
         <button class="load-more" type="button">load more</button>
       </section>
         `
-    )
-}
+  );
+};
 
 const createTaskTemplate = () => {
   return (
